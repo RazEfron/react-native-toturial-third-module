13e3a7151dd1494ac9a4f2a1853ffdf1545b2ce7
Raz <razefron01@gmail.com>
2020-07-08 17:48:45 +0300

    header

--- components/Header.js
+++ components/Header.js
@@ -3,8 +3,8 @@ import { View, Text, StyleSheet } from 'react-native';
 
 const Header = props => {
     return (
-        <View style={}>
-            <Text>
+        <View style={styles.header}>
+            <Text style={styles.headerTitle}>
                 {props.title}
             </Text>
         </View>
@@ -12,7 +12,19 @@ const Header = props => {
 }
 
 const styles = StyleSheet.create({
+    header: {
+        width: '100%',
+        height: 90,
+        paddingTop: 36,
+        backgroundColor: '#f7287b',
+        alignItems: 'center',
+        justifyContent: 'center',
 
+    },
+    headerTitle: {
+        color: 'black',
+        fontSize: 18
+    }
 })
 
 export default Header;
