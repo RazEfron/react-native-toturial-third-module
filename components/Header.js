1f615664b32e4dfe4b2f43132534264bd09ed40a
Raz <razefron01@gmail.com>
2020-07-08 17:42:22 +0300

    initial commit

---
+++ components/Header.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+
+const Header = props => {
+    return (
+        <View style={}>
+            <Text>
+                {props.title}
+            </Text>
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({
+
+})
+
+export default Header;
